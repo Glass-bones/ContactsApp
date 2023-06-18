7c96f00928915657451a66ae7d98058850024df8
ZMEY <zmey.ilya@gmail.com>
2023-06-18 16:31:28 +0700

    NewLogic(ContactForm)Добавлена базовая логика окна ввода контакта

--- src/ContactsApp/ContactsApp.View/ContactForm.cs
+++ src/ContactsApp/ContactsApp.View/ContactForm.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ContactsApp.Model;
 
 namespace ContactsApp.View
 {
@@ -17,10 +18,24 @@ namespace ContactsApp.View
         {
             InitializeComponent();
         }
+        private Contact _contact;
+        private string _phoneNumberError;
+
+        private void UpdateForm()
+        {
+            FullNameTextBox.Text = _contact.GetFullName();
+            EmailTextBox.Text = _contact.GetEmail();
+            PhoneTextBox.Text = _contact.GetPhoneNumber();
+            BirthDateTimePicker.Value = _contact.GetBirthDate();
+            VKTextBox.Text = _contact.GetVK();
+        }
 
         private void ContactForm_Load(object sender, EventArgs e)
         {
-
+            _contact = new Contact("john Doe", "@no.mail", "+7 (000) 000-00-00", "", DateTime.Today);
+            BirthDateTimePicker.MaxDate = DateTime.Today;
+            _phoneNumberError = "";
+            UpdateForm();
         }
 
         private void AddPhotoButton_MouseEnter(object sender, EventArgs e)
@@ -33,16 +48,6 @@ namespace ContactsApp.View
             AddPhotoButton.Image = Properties.Resources.add_photo_32x32_gray;
         }
 
-        private void OkAddButton_Click(object sender, EventArgs e)
-        {
-            if (FullNameTextBox.Text.Length <= 0) return;
-            else if (PhoneTextBox.Text == "")
-                Close();
-            else if (!Regex.IsMatch(PhoneTextBox.Text, "^((8|\\+7)[\\- ]?)?(\\(?\\d{3}\\)?[\\- ]?)?[\\d\\- ]{7,10}$"))
-                return;
-            else Close();
-        }
-
         private void CancelAddButton_Click(object sender, EventArgs e)
         {
             Close();
@@ -51,17 +56,61 @@ namespace ContactsApp.View
         private void FullNameTextBox_TextChanged(object sender, EventArgs e)
         {
             if (FullNameTextBox.Text.Length <= 0) return;
-            else if (char.IsLower(Convert.ToChar(FullNameTextBox.Text.Substring(0, 1))))
+            else
             {
-                FullNameTextBox.Text = FullNameTextBox.Text.Substring(0, 1).Replace(FullNameTextBox.Text.Substring(0, 1), FullNameTextBox.Text.Substring(0, 1).ToUpper()) + FullNameTextBox.Text.Substring(1, FullNameTextBox.Text.Length - 1);
-                FullNameTextBox.SelectionStart = 1;
+                if (char.IsLower(Convert.ToChar(FullNameTextBox.Text.Substring(0, 1))))
+                {
+                    FullNameTextBox.Text = FullNameTextBox.Text.Substring(0, 1).Replace(FullNameTextBox.Text.Substring(0, 1), FullNameTextBox.Text.Substring(0, 1).ToUpper()) + FullNameTextBox.Text.Substring(1, FullNameTextBox.Text.Length - 1);
+                    FullNameTextBox.SelectionStart = 1;
+                }
+                _contact.SetFullName(FullNameTextBox.Text);
             }
         }
 
         private void PhoneTextBox_TextChanged(object sender, EventArgs e)
         {
+            try
+            {
+                _contact.SetPhoneNumber(PhoneTextBox.Text);
+                PhoneTextBox.BackColor = Color.White;
+                _phoneNumberError = "";
+            }
+            catch
+            {
+                PhoneTextBox.BackColor = Color.LightPink;
+                _phoneNumberError = "Invalid phone number.";
+            }
+        }
 
-            //if(PhoneTextBox.Text.)
+        private void BirthDateTimePicker_ValueChanged(object sender, EventArgs e)
+        {
+            _contact.SetBirthDate(BirthDateTimePicker.Value);
+        }
+
+        private void VKTextBox_TextChanged(object sender, EventArgs e)
+        {
+            _contact.SetVK(VKTextBox.Text);
+        }
+
+        private void EmailTextBox_TextChanged(object sender, EventArgs e)
+        {
+            _contact.SetEmail(EmailTextBox.Text);
+        }
+        private bool CheckFormOnErrors()
+        {
+            if (_phoneNumberError.Length > 0)
+            {
+                MessageBox.Show(_phoneNumberError, "Error!", MessageBoxButtons.OK);
+                return false;
+            }
+            else return true;
+        }
+
+        //тк эта форма - диалог, проверка на ошибки происходит только перед выходом, а не при нажатии кнопки
+        private void ContactForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (this.DialogResult == DialogResult.OK && !CheckFormOnErrors())
+                e.Cancel = true;
         }
     }
 }
